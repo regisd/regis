a7b03df2c7f5bc4c9fa0348c4e47359dc47bfb92
regis <regis@localhost>
2010-03-27 17:30:39 +0100

    Migration from SVN repo at revision 12

---
+++ chrome-extensions/allowrightclick/rightclick.js
@@ -0,0 +1,30 @@
+// if (document.addEventListener)
+//   document.addEventListener("DOMContentLoaded", walkmydog, false)
+var timer = window.setInterval(function() {
+            if (/loaded|complete/.test(document.readyState)){
+                window.clearInterval(timer);
+                enableContextMenu();
+            }
+          }, 30);
+	  
+function enableContextMenu() {
+  console.log("allow right click on "+window.location);
+  void(document.ondragstart=null);
+  void(document.onselectstart=null);
+  void(document.onclick=null);
+  void(document.onmousedown=null);
+  void(document.onmouseup=null);
+  void(document.oncontextmenu=null);
+  void(document.body.oncontextmenu=null);
+  removeContextOn("img");
+  removeContextOn("td");
+}
+
+function removeContextOn(elt) {
+  var elements=document.getElementsByTagName(elt);
+  //for (var e in elements) {
+  for (var i=0;i<elements.length;i++) {
+    e=elements[i];
+    void(e.oncontextmenu=null);    
+  }
+}

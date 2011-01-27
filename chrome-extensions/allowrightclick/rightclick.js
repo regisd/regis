5b4ffa00f62a81b8d1ecb842da9f8ba27de082b5
Regis Decamps <decamps@users.sf.net>
2011-01-28 00:14:51 +0100

    fixes issue 8 : I just had to apply my current process on div elements to work on Youtube

--- chrome-extensions/allowrightclick/rightclick.js
+++ chrome-extensions/allowrightclick/rightclick.js
@@ -23,8 +23,8 @@ function enableContextMenu() {
   removeContextMenuOn(document);
   removeContextOnAll("img");
   removeContextOnAll("td");
-  
-  miscHacks();
+  removeContextOnAll("div");
+  //miscHacks();
 }
 
 function removeContextOnAll(eltName) {
@@ -36,8 +36,8 @@ function removeContextOnAll(eltName) {
 }
 
 function removeContextMenuOn(elt) {
-	// I don't think this is still usefull, yet I won't take risk to break the extension on many sites
-    void(elt.oncontextmenu=null);
+	//hope I won't break the extension anywhere
+	//void(elt.oncontextmenu=null);
     //more general than elt.oncontextmenu	
     elt.addEventListener("contextmenu", bringBackDefault, false);
 }
@@ -48,25 +48,11 @@ function bringBackDefault(event) {
 
 function miscHacks() {
 	// see flickr.css
-	// Cannot readthe localStorage in this context. Need to send a message to the background page.
+	// Cannot read the localStorage in this context. Need to send a message to the background page.
 	if (document.domain.match("youtube")) {
 		chrome.extension.sendRequest({method: "optionhackYoutube"}, function(response) {
 			if(response.value)
 				youtubeHack();
 		});
 	}
-}
-
-/* Youtube oncontextmenu is anonymous and cannot be removed with the previous technique.
- * The workaround is to clone the node.
- * This hack comes from http://mortalpowers.com/news/youtube-freedom-version-3 
- */
-function youtubeHack() {
-	vb=document.getElementsByClassName('video-blocker')[0];
-	if(vb) vb.style['display'] = 'none';
-	videoContent = document.getElementsByClassName('video-content')[0];
-	p = videoContent.parentElement;
-	// cloning a node does not copy the event handlers
-	cleanVideoContent = videoContent.cloneNode(true);
-	p.replaceChild(cleanVideoContent,videoContent);
 }
